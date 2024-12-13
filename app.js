6b126100212d4db81aec94c4a2f7bf487c59dd5a
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2024-12-13 12:14:11 +0530

    changes

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
-// const port = 8000;
-// const path = require("path")
-// const methodOverride = require("method-override")
-// const ejsMate = require("ejs-mate");
-// const ExpressError = require("./utils/ExpressErr");
-// const listings = require("./routes/listing")
-// const reviews = require("./routes/review")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 8000;
+const path = require("path")
+const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressErr");
+const listings = require("./routes/listing")
+const reviews = require("./routes/review")
 
 
 
-// // ejs
-// app.set("view engine", "ejs")
-// app.set("views", path.join(__dirname, "views"))
-// app.use(express.static(path.join(__dirname, "public")))
-// app.use(express.urlencoded({extended:true}))
-// app.use(methodOverride("_method"))
-// app.engine("ejs", ejsMate)
+// ejs
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
+app.engine("ejs", ejsMate)
  
-// const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
-// main()
-//   .then(() => {
-//     console.log(`connected to DB`);
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
+main()
+  .then(() => {
+    console.log(`connected to DB`);
     
-//   })
-//   .catch((err) => console.log(err));
-// async function main() {
-//   await mongoose.connect(MONGO_URL);
-// }
+  })
+  .catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
 
-// // listings
+// listings
 
-// app.use("/listings", listings)
+app.use("/listings", listings)
 
-// // reviews
-// app.use("/listings/:id/reviews", reviews)
+// reviews
+app.use("/listings/:id/reviews", reviews)
 
 
-// app.all("*", (req,res,next) => {
-//   next((new ExpressError(404,"Page not found")))
-// })
+app.all("*", (req,res,next) => {
+  next((new ExpressError(404,"Page not found")))
+})
 
-// app.use((err, req, res, next) => {
-//   let { statusCode, message } = err;
-//   if (!statusCode) statusCode = 500;
-//   console.log(err);
+app.use((err, req, res, next) => {
+  let { statusCode, message } = err;
+  if (!statusCode) statusCode = 500;
+  console.log(err);
   
-//   res.status(statusCode).render("listings/errors", { err });
-// })
+  res.status(statusCode).render("listings/errors", { err });
+})
 
-// app.listen(port, () => {
-//   console.log(`app is running on port no ${port}`);
+app.listen(port, () => {
+  console.log(`app is running on port no ${port}`);
   
-// })
+})
 
-// module.exports = app;
+module.exports = app;
